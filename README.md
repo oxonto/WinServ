d47e5e572e39fdb982a236d2f8d755d13a607331
oxonto <antoine.geslin123@gmail.com>
2025-04-01 16:43:20 +0200

    modification script and readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-## In the CSV file, the first column is the last name "nom", the second is the first name "prenom", and the third is the OU name "ou"
-## The CSV file must be in the same directory as the script
+Le présent script simplifie la création des OU et des Utilisateurs dans un Windows Server (tester sur Windows Server 2022).
+Il fonctionne en doublon avec un fichier CSV pour l'import des OU et des Utilisateurs.
+
+Dans le fichier CSV, la première colonne est le nom de famille "nom", la deuxième est le prénom "prenom" et la troisième est le nom de l'unité d'organisation "ou".
+Le fichier CSV doit se trouver dans le même répertoire que le script pour une utilisation simplifier.
--- script.ps1
+++ script.ps1
@@ -1,21 +1,22 @@
-#In the CSV file, the first column is the last name "nom", the second is the first name "prenom", and the third is the OU name "ou"
-#The CSV file must be in the same directory as the script
-
+#Variables de configuration
 $path = ".\import.csv"
 $domain = "h35.lan"
+$password = "Zqsd@2024"
 
 
-
+#Variables d'éxecution
 $users = import-csv -path $path -delimiter ","
 $ous = $users | Select-Object -ExpandProperty ou | Sort-Object -Unique
 $DC1, $DC2 = $domain -split "\."
 $DC = "DC=$DC1,DC=$DC2"
 
+#Création des OU
 foreach($ou in $ous)
 {
     New-ADOrganizationalUnit -Name "$ou" -Path "$DC" -ProtectedFromAccidentalDeletion $false
 }
 
+#Création des utilisateurs
 foreach($user in $users)
 {
     $nom = $user.nom
@@ -26,7 +27,7 @@ foreach($user in $users)
     $idSAM = $prenom.substring(0,1) + $nom
     $id = $idSAM + "@$domain"
 
-    $pass= ConvertTo-SecureString "Zqsd@2024" -AsPlaintext -Force
+    $pass= ConvertTo-SecureString "$password" -AsPlaintext -Force
 
     New-ADUser -name $idSAM -DisplayName $nomComplet -givenname $prenom -surname $nom -Path "OU=$unitgroup,$DC" -UserPrincipalName $id -AccountPassword $pass -Enabled $true
 }
