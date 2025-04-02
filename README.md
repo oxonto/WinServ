8c8127eed288f92c1312e82837083c6e071c4c8b
oxonto <antoine.geslin123@gmail.com>
2025-04-02 11:39:19 +0200

    add password in csv

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 Le présent script simplifie la création des OU et des Utilisateurs dans un Windows Server (tester sur Windows Server 2022).
-Il fonctionne en doublon avec un fichier CSV pour l'import des OU et des Utilisateurs.
+Il fonctionne en doublon avec un fichier CSV pour la création des OU et l'import des Utilisateurs.
 
-Dans le fichier CSV, la première colonne est le nom de famille "nom", la deuxième est le prénom "prenom" et la troisième est le nom de l'unité d'organisation "ou".
+Le fichier CSV nécessite 4 colones : "prenom" pour le prénom, "nom" pour le nom de famille, "password" pour le mot de passe, "ou" pour l'unité d'organisation.
 Le fichier CSV doit se trouver dans le même répertoire que le script pour une utilisation simplifier.
--- script.ps1
+++ script.ps1
@@ -1,27 +1,28 @@
 #Variables de configuration
 $path = ".\import.csv"
 $domain = "h35.lan"
-$password = "Zqsd@2024"
 
 
 #Variables d'éxecution
 $users = import-csv -path $path -delimiter ","
-$ous = $users | Select-Object -ExpandProperty ou | Sort-Object -Unique
+$listOU = $users | Select-Object -ExpandProperty ou | Sort-Object -Unique
 $DC1, $DC2 = $domain -split "\."
 $DC = "DC=$DC1,DC=$DC2"
 
 #Création des OU
-foreach($ou in $ous)
+foreach($eachOU in $listOU)
 {
-    New-ADOrganizationalUnit -Name "$ou" -Path "$DC" -ProtectedFromAccidentalDeletion $false
+    New-ADOrganizationalUnit -Name "$eachOU" -Path "$DC" -ProtectedFromAccidentalDeletion $false
 }
 
 #Création des utilisateurs
 foreach($user in $users)
 {
     $nom = $user.nom
-    $prenom= $user.prenom
-    $unitgroup = $user.ou
+    $prenom = $user.prenom
+    $ou = $user.ou
+    $password = $user.password
+
 
     $nomComplet = $prenom + " " +$nom
     $idSAM = $prenom.substring(0,1) + $nom
@@ -29,5 +30,5 @@ foreach($user in $users)
 
     $pass= ConvertTo-SecureString "$password" -AsPlaintext -Force
 
-    New-ADUser -name $idSAM -DisplayName $nomComplet -givenname $prenom -surname $nom -Path "OU=$unitgroup,$DC" -UserPrincipalName $id -AccountPassword $pass -Enabled $true
+    New-ADUser -name $idSAM -DisplayName $nomComplet -givenname $prenom -surname $nom -Path "OU=$ou,$DC" -UserPrincipalName $id -AccountPassword $pass -Enabled $true
 }
